9432342e9ef16d3f09d5d7230e8a0e8b7c44d6e5
Maksym Andreiev <maxoggyha@gmail.com>
2024-04-15 13:59:47 +0900

    Add exercise 2 of week 2

---
+++ Week2/led_photoresistor.c
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+#define Threshold 0.1f
+
+// main() runs in its own thread in the OS
+int main()
+{
+    AnalogIn photo(A0);
+    DigitalOut led(D12);
+    while (true) {
+        if (photo.read() > Threshold){
+            led = 0;
+        } else {
+            led = 1;
+        }
+        printf("ADC_read = %f\n", photo.read());
+        ThisThread::sleep_for(100ms);
+    }
+}
+
